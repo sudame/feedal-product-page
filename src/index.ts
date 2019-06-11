b9ae9fdea7f3d78194a84cbd2b5d15f0b742ec10
Mikihiro SUDA <su.glasses.923@gmail.com>
2019-06-12 00:56:53 +0900

    add: シャレオツエフェクト

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,30 @@
-const hoge: string = "boobar";
+const logo = document.getElementById("logo");
+const dummy = document.createElement('div');
+logo.parentNode.insertBefore(dummy, logo.nextSibling);
 
-console.log(hoge);
+const initX = logo.offsetLeft;
+const initY = logo.offsetTop;
+const initHeight = logo.offsetHeight;
+const initWidth = logo.offsetWidth;
 
-class Software {
-    name: string;
-    id: number;
+logo.style.position = 'fixed';
+logo.style.top = `${initY}px`;
+logo.style.left = `${initX}px`;
+dummy.style.height = `${initHeight}px`;
+dummy.style.width = `${initWidth}px`;
 
-    constructor(name: string, id: number) {
-        this.name = name;
-        this.id = id;
+const dHeight = (initHeight - 25) / initY;
+const dX = (initX + 25) / initY;
+
+console.log(dHeight);
+
+window.addEventListener('scroll', () => {
+    const pageY = window.pageYOffset;
+    const y = logo.offsetTop;
+
+    if (y > 25 || pageY + 25 < initY) {
+        logo.style.top = `${initY - window.pageYOffset}px`;
+        logo.style.height = `${Math.max((initY - pageY) * dHeight, 25)}px`;
+        logo.style.left = `${Math.max((initY - pageY) * dX, 25)}px`;
     }
-}
-
-const sft: Software = new Software('aaa', 3);
-console.log(sft.name);
+});
