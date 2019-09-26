eca3f2a53f2b283528f5cdb646995cfcb9e70d7a
Mikihiro SUDA <su.glasses.923@gmail.com>
2019-09-26 11:24:37 +0900

    fix: outer link

--- src/common/outbounds.ts
+++ src/common/outbounds.ts
@@ -11,13 +11,18 @@ document.addEventListener("DOMContentLoaded", () => {
       target.indexOf("http") === 0 &&
       target.indexOf("https://feedal.com") < 0
     ) {
-      link.addEventListener("click", () => {
+      link.addEventListener("click", (event) => {
+        event.preventDefault();
         gtag("event", "click", {
           event_category: "outbound",
           event_label: target,
           transport_type: "beacon",
           event_callback: () => {
-            document.location.href = target;
+            if (link.getAttribute('target') === "_blank") {
+              window.open(target, '_blank');
+            } else {
+              document.location.href = target;
+            }
           }
         });
       });
